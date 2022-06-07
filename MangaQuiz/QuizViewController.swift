bd90676b2f5e967a20ca271a04fa07ecfe5a1170
森田陸斗 <mrmrt1121@gmail.com>
2022-06-07 14:15:51 +0900

    csvの読み込みと正誤判定処理の追加

--- MangaQuiz/QuizViewController.swift
+++ MangaQuiz/QuizViewController.swift
@@ -8,14 +8,59 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    @IBOutlet weak var quizNumberLabel: UILabel!
+    @IBOutlet weak var quizTextView: UITextView!
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
+    @IBOutlet weak var answerButton4: UIButton!
+    
+    var csvArray: [String] = []
+    var quizArray: [String] = []
+    var quizCount = 0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // csvファイルを実際に読み込み
+        csvArray = loadCSV(fileName: "quiz")
+        
+        quizArray = csvArray[quizCount].components(separatedBy: ",")
+        
+        quizNumberLabel.text = "第\(quizCount + 1)問"
+        
+        // 問題文
+        quizTextView.text = quizArray[0]
+        answerButton1.setTitle(quizArray[2], for: .normal)
+        answerButton2.setTitle(quizArray[3], for: .normal)
+        answerButton3.setTitle(quizArray[4], for: .normal)
+        answerButton4.setTitle(quizArray[5], for: .normal)
+    
     }
     
     @IBAction func btnAction(sender: UIButton) {
-        print(sender.tag)
+        if sender.tag == Int(quizArray[1]) {
+            print("正解")
+        } else {
+            print("不正解")
+        }
+    }
+    
+    // csvファイルを読み込むメソッド
+    func loadCSV(fileName: String) -> [String] {
+        // Bundle.main.path(): パッケージ化されたリソースの保存領域を取得
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            //csvBundleのパスを読み込み、UTF8に文字コード変換して、NSStringに格納
+            let csvData = try String(contentsOfFile: csvBundle, encoding: String.Encoding.utf8)
+            //改行コードが"\r"で行なわれている場合は"\n"に変更する
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            //"\n"の改行コードで区切って、配列csvArrayに格納する
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
+        } catch {
+            print("エラー")
+        }
+        return csvArray
     }
 }
