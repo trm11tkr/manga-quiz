95c2cddc59b03aca3587b132a2b36c5cd9f03679
森田陸斗 <mrmrt1121@gmail.com>
2022-06-07 15:08:43 +0900

    正解数の表示

--- MangaQuiz/QuizViewController.swift
+++ MangaQuiz/QuizViewController.swift
@@ -18,6 +18,7 @@ class QuizViewController: UIViewController {
     var csvArray: [String] = []
     var quizArray: [String] = []
     var quizCount = 0;
+    var correctCount = 0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,11 +38,14 @@ class QuizViewController: UIViewController {
     
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let scoreVC = segue.destination as! ScoreViewController
+        scoreVC.correct = correctCount
+    }
+    
     @IBAction func btnAction(sender: UIButton) {
         if sender.tag == Int(quizArray[1]) {
-            print("正解")
-        } else {
-            print("不正解")
+            correctCount += 1
         }
         nextQuiz()
     }
